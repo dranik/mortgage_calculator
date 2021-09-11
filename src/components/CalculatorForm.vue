01b8f553372c82e6a6e1013a798707573b54cdcc
Stan Pankov <spankov@hgs.cloud>
2021-09-11 15:23:06 +0200

    Layout

---
+++ src/components/CalculatorForm.vue
@@ -0,0 +1,91 @@
+<template>
+  <v-form>
+    <v-container>
+      <v-row dense>
+        <v-col cols="6">
+          <v-label><span class="calculator-form__label">Property Purchase Price</span></v-label>
+          <v-text-field
+            placeholder="e.g 200000"
+            prepend-inner-icon="mdi-currency-eur"
+            outlined
+          ></v-text-field>
+        </v-col>
+        <v-col cols="6">
+          <v-label><span class="calculator-form__label">Total savings</span></v-label>
+          <v-text-field
+            placeholder="e.g 50000"
+            prepend-inner-icon="mdi-currency-eur"
+            outlined
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <v-row dense>
+        <v-col cols="6">
+          <v-label><span class="calculator-form__label">Real estate commission</span></v-label>
+          <v-select
+            :items="items"
+            placeholder="Please select"
+            outlined
+          ></v-select>
+        </v-col>
+        <v-col cols="6">
+          <v-label><span class="calculator-form__label">Annual payment rate(%)</span></v-label>
+          <v-text-field
+            placeholder="e.g 2.0"
+            outlined
+          >
+            <v-icon
+              slot="append"
+              color="secondary"
+              @click="increaseAnnualPayment"
+            >
+              mdi-plus
+            </v-icon>
+            <v-icon
+              slot="prepend-inner"
+              color="secondary"
+              @click="decreaseAnnualPayment"
+            >
+              mdi-minus
+            </v-icon>
+          </v-text-field>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script>
+  export default {
+    name: 'CalculatorForm',
+    data: () => ({
+      items: [
+        {
+          value: true,
+          text: 'Yes'
+        },
+        {
+          value: false,
+          text: 'No'
+        }
+      ]
+    }),
+    methods: {
+      increaseAnnualPayment() {
+
+      },
+      decreaseAnnualPayment() {
+
+      }
+    }
+  }
+</script>
+<style lang="scss">
+@import '@/mixins/colors.scss';
+
+.calculator-form__label {
+  font-weight: bold;
+  padding-bottom: 16px;
+  color: $c-dark;
+}
+</style>
---
+++ src/pages/Calculator.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-container class="calculator__container">
+    <v-row>
+      <v-col cols="12">
+        <v-card
+          elevation="6"
+        >
+          <v-card-title class="calculator__title">Mortgage Calculator</v-card-title>
+          <v-card-text><CalculatorForm></CalculatorForm></v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+
+            <v-btn
+              large
+              color="primary"
+              width="150"
+            >
+              Calculate
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col cols="6">
+        <v-card
+          elevation="6"
+        >
+          <v-card-title class="calculator__result-label">Implied loan</v-card-title>
+          <v-card-text class="calculator__result-value">9001,44€</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="6">
+        <v-card
+          elevation="6"
+        >
+          <v-card-title class="calculator__result-label">Loan to value</v-card-title>
+          <v-card-text class="calculator__result-value">9001,44%</v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  import CalculatorForm from '@/components/CalculatorForm.vue';
+
+  export default {
+    name: 'Calculator',
+    data: () => ({
+    }),
+    components:{
+      CalculatorForm
+    }
+  }
+</script>
+
+<style lang="scss">
+  @import '@/mixins/colors.scss';
+
+  .calculator__container {
+    max-width: 600px;
+  }
+
+  .v-card__title.calculator__title {
+    font-size: 24px;
+    color: $c-dark;
+  }
+
+  .v-card__title.calculator__result-label {
+    font-size: 20px;
+    color: $c-dark;
+  }
+
+  .theme--light.v-card > .v-card__text.calculator__result-value {
+    font-size: 48px;
+    font-weight: bold;
+    padding-bottom: 36px;
+    color: $c-dark;
+  }
+
+  .calculator__container .v-card__actions {
+    padding: 16px 24px;
+  }
+</style>
